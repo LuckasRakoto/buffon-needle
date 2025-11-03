1045fed79282a67c1dc7926df5b23f83f49bcead
Luckas Rakoto <luckasrakoto@gmail.com>
2025-11-03 12:30:50 -0500

    Shader.h class init

--- include/shader/shader.h
+++ include/shader/shader.h
@@ -1,1 +1,15 @@
-class Shader {};
+#pragma once
+
+#include <string>
+
+class Shader {
+public:
+  unsigned int ID;
+  Shader(const char *vertexPath, const char *fragmentPath);
+  ~Shader();
+
+private:
+  unsigned int compileVertexShader(std::string vertShaderSource);
+  unsigned int compileFragmentShader(std::string fragShaderSource);
+  void buildProgram(unsigned int vertShader, unsigned int fragShader);
+};
