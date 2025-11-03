ad7c360eecf047fe66c7699072dc901687a3f1e4
Luckas Rakoto <luckasrakoto@gmail.com>
2025-11-02 22:27:54 -0500

    Spawn Window functions

--- include/window/window.h
+++ include/window/window.h
@@ -1,6 +1,17 @@
-class Window {
-  private:
-    static void loadGL();
+#pragma once
+#include <string>
 
-  public:
+const int WIDTH = 1200;
+const int HEIGHT = 800;
+
+class Window {
+private:
+  int width;
+  int height;
+  std::string title;
+
+  void loadGL();
+
+public:
+  Window(int w, int h, std::string title);
 };
--- src/window/window.cpp
+++ src/window/window.cpp
@@ -7,3 +7,8 @@ void Window::loadGL() {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 };
+
+Window::Window(int w, int h, std::string title)
+    : width(w), height(h), title(title) {
+  glfwCreateWindow(width, height, title.c_str(), NULL, NULL);
+}
