b5918faf8e66f42cd2acce18639fabf94730590c
Luckas Rakoto <luckasrakoto@gmail.com>
2025-11-02 22:11:01 -0500

    Window & loadGL

---
+++ include/window/window.h
@@ -0,0 +1,6 @@
+class Window {
+  private:
+    static void loadGL();
+
+  public:
+};
---
+++ src/window/window.cpp
@@ -0,0 +1,9 @@
+#include <window/window.h>
+#include "GLFW/glfw3.h"
+
+void Window::loadGL() {
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+};
