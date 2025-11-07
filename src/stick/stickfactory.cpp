3debaf7b191bd3334e206e688c954875c7394eae
Luckas Rakoto <luckasrakoto@gmail.com>
2025-11-07 10:04:40 -0500

    Stick facotyr new stick

---
+++ src/stick/stickfactory.cpp
@@ -0,0 +1,25 @@
+#include "stick/stickfactory.h"
+#include <cmath>
+#include <cstdlib>
+
+StickFactory::StickFactory(float l) : length(l) {
+}
+
+Stick *StickFactory::newStick() {
+  float x = (rand() - 1.0f) / RAND_MAX;
+  float y = (rand() - 1.0f) / RAND_MAX;
+  float theta = (static_cast<float>(rand()) / RAND_MAX) * 360.0f;
+  return new Stick(getStart(x, y, theta), getEnd(x, y, theta));
+}
+
+Position StickFactory::getStart(float x, float y, float theta) {
+  return {.x = x + std::cos(theta) * length,
+          .y = y + std::sin(theta) * length,
+          .z = 1.0f};
+}
+
+Position StickFactory::getEnd(float x, float y, float theta) {
+  return {.x = x - std::cos(theta) * length,
+          .y = y - std::sin(theta) * length,
+          .z = 1.0f};
+}
