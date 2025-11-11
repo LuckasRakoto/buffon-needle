775af584fbe49539ea6fd3db0206ba4eef87eb21
Luckas Rakoto <luckasrakoto@gmail.com>
2025-11-11 11:06:07 -0500

    STICKS_COLOR typo

--- include/color/color.h
+++ include/color/color.h
@@ -1,5 +1,5 @@
 #include <array>
 
-extern constinit const std::array<float, 3> STICK_COLOR;
+extern constinit const std::array<float, 3> STICKS_COLOR;
 extern constinit const std::array<float, 3> BACKGROUND_COLOR;
 extern constinit const std::array<float, 3> BACKGROUND_SLITS_COLOR;
