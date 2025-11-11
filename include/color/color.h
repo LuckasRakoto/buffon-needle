b5f4554b797ff875225229cd9cd2929413e589f6
Luckas Rakoto <luckasrakoto@gmail.com>
2025-11-11 10:13:15 -0500

    wannabe private functions in color.h

--- include/color/color.h
+++ include/color/color.h
@@ -1,18 +1,18 @@
 #include <array>
 
-constexpr float toFloat(uint8_t c) {
+constexpr float _toFloat(uint8_t c) {
   return c / 255.0f;
 }
 
-constexpr std::array<float, 3> buildColor(const std::array<uint8_t, 3> &rgb) {
+constexpr std::array<float, 3> _buildColor(const std::array<uint8_t, 3> &rgb) {
   std::array<float, 3> res{};
   for (size_t i = 0; i < rgb.size(); i++) {
-    res[i] = toFloat(rgb[i]);
+    res[i] = _toFloat(rgb[i]);
   }
   return res;
 }
 
-constexpr std::array<float, 3> STICKS_COLOR = buildColor({208, 171, 160});
-constexpr std::array<float, 3> BACKGROUND_COLOR = buildColor({222, 196, 161});
+constexpr std::array<float, 3> STICKS_COLOR = _buildColor({208, 171, 160});
+constexpr std::array<float, 3> BACKGROUND_COLOR = _buildColor({222, 196, 161});
 constexpr std::array<float, 3> BACKGROUND_SLITS_COLOR =
-  buildColor({237, 207, 142});
+  _buildColor({237, 207, 142});
