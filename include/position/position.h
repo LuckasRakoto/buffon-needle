956e4e860cbaa03dc3ec955340a1fca7049947eb
Luckas Rakoto <luckasrakoto@gmail.com>
2025-11-18 16:24:10 -0500

    offset position

--- include/position/position.h
+++ include/position/position.h
@@ -3,7 +3,10 @@
 
 class Position {
 public:
+  Position(std::array<float, 3> pos);
   std::array<float, 3> unpack() const;
+
+  Position offset_xy(Position from, float angle, float step);
 
 private:
   float x, y, z;
--- src/position/position.cpp
+++ src/position/position.cpp
@@ -1,6 +1,19 @@
 #include "position/position.h"
+#include <array>
 #include <print>
 
-std::array<float, 3> Position::unpack() {
+Position::Position(std::array<float, 3> pos) : x(pos[0]), y(pos[1]), z(pos[2]) {
+}
+
+std::array<float, 3> Position::unpack() const {
   return {x, y, z};
+}
+
+Position Position::offset_xy(Position from, float angle, float step) {
+  auto [x, y, z] = from.unpack();
+  return Position({
+      x + step * cos(angle), //
+      y + step * sin(angle), //
+      z                      //
+  });
 }
